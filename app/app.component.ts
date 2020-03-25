bd2322b9aa748fa3656d3031d03944ec8148b441
matheusAle <m.matheus.ale@gmail.com>
2020-03-25 14:14:29 -0300

    [force: true] erwer

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import Faker from "faker";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  name = 'Angular 5';
+
+  table1: {
+    data: object[];
+    headers: string[];
+  } = { data: [], headers: [] };
+
+  constructor() {
+    this.addColumn();
+    this.addColumn();
+    this.addColumn();
+    this.addData();
+    this.addData();
+    this.addData();
+  }
+
+  addData() {
+    this.table1 = {
+      ...this.table1,
+      data: [...this.table1.data, {}].map((c: object) => 
+        this.table1.headers.reduce((acc, h) => ({ ...acc, [h]: Faker.random.word() }), {})
+      ),
+    }
+  }
+
+  addColumn() {
+    const newColName = `col-${this.table1.headers.length}`;
+    const headers = [...this.table1.headers, newColName];
+    this.table1 = {
+      ...this.table1,
+      headers,
+    };
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TableModule } from './table/table.module'
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, TableModule ],
+  imports:      [ BrowserModule, FormsModule, TableModule, DragDropModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
